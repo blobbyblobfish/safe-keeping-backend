20945e6b32fa79c66eb2979ff8e095be7a5919d1
blobbyblobfish <aywang6@gmail.com>
2020-08-06 19:24:51 -0400

    Diary Card serializer update

--- app/serializers/diary_card_serializer.rb
+++ app/serializers/diary_card_serializer.rb
@@ -1,5 +1,5 @@
 class DiaryCardSerializer < ActiveModel::Serializer
-    attributes :id, :thoughts, :feelings, :created_at
     has_many :diary_card_trackers
+    attributes :id, :thoughts, :feelings, :created_at
 end
   
