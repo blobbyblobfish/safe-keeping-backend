4d798fccfebcfade6bb673cb7ce11574f78afb5c
blobbyblobfish <aywang6@gmail.com>
2020-08-06 19:24:37 -0400

    Custom routing

--- app/serializers/diary_card_serializer.rb
+++ app/serializers/diary_card_serializer.rb
@@ -1,5 +1,5 @@
 class DiaryCardSerializer < ActiveModel::Serializer
-    attributes :thoughts, :feelings, :created_at
+    attributes :id, :thoughts, :feelings, :created_at
     has_many :diary_card_trackers
 end
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :diary_card_trackers
   resources :trackers
-  resources :diary_cards
+  # resources :diary_cards
   
   get '/users/:id', to: 'users#show'
+  post '/diary_cards', to: 'diary_cards#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
