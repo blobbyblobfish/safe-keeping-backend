c3ef2ffba96f1104472bfd353009a1fcdc3bbe24
blobbyblobfish <aywang6@gmail.com>
2020-08-06 19:14:48 -0400

    Diary Card creation

--- app/controllers/diary_cards_controller.rb
+++ app/controllers/diary_cards_controller.rb
@@ -1,2 +1,14 @@
 class DiaryCardsController < ApplicationController
+
+    def create
+        @diary_card = DiaryCard.create(diary_card_params)
+        render json: @diary_card
+    end
+
+    private
+
+    def diary_card_params
+        params.require(:diary_card).permit(:user_id, :thoughts, :feelings)
+    end
+    
 end
