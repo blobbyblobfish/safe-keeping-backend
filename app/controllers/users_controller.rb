4b665ad66199b7d562469df7ec92a26496035984
blobbyblobfish <aywang6@gmail.com>
2020-08-11 15:40:22 -0400

    Send serialized user json

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,18 +7,21 @@ class UsersController < ApplicationController
 
     def show
         @user = User.find(params[:id])
-
         render json: @user
     end
 
     def create
         @user = User.create(create_user_params)
 
+        # Seed users with initial tracker and coping skills
+        Tracker.create(user: @user, name: "Mood")
+        CopingSkill.create(user: @user, name: "Test", description: "Lorem ipsum...", directions: "Step 1, 2, 3.", attempts: 0, successful_attempts: 0)
+
         if @user.valid?
             token = encode_token({user_id: @user.id})
 
             render json: {
-                user: @user,
+                user: UserSerializer.new(@user),
                 token: token
             }
 
@@ -27,6 +30,21 @@ class UsersController < ApplicationController
                 error: "Error. Registration not successful."
             }
         end
+
+    end
+
+    def update
+        @user.update(update_user_params)
+        render json: @user
+    end
+
+    def destroy
+        @user.destroy()
+        render json: @user
+    end
+
+    def find
+        @user = User.find(params[:id])
     end
 
     def login
@@ -34,6 +52,7 @@ class UsersController < ApplicationController
 
         if @user && @user.authenticate(params[:password])
             token = encode_token({user_id: @user.id})
+
             render json: {
               user: UserSerializer.new(@user),
               token: token
@@ -42,18 +61,7 @@ class UsersController < ApplicationController
         else
             render json: {error: "Cannot login."}
         end
-    end
 
-    def update
-        
-    end
-
-    def destroy
-        
-    end
-
-    def find
-        @user = User.find(params[:id])
     end
 
     private
@@ -62,4 +70,7 @@ class UsersController < ApplicationController
         params.permit(:email, :first_name, :password)
     end
 
+    def update_user_params
+        params.require(:user).permit(:email, :first_name)
+    end
 end
