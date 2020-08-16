2c1f56ba5ff79188de0a57e51bed6a764b3420e6
blobbyblobfish <aywang6@gmail.com>
2020-08-16 19:14:17 -0400

    Include created_at in user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :email
+  attributes :id, :first_name, :email, :created_at
   has_many :coping_skills
   has_many :diary_cards
   has_many :emergency_contacts
